ed515aa5698e08353b21f5f045ebf457ece082bd
Joey McCoy <joey_mccoy@live.com>
2016-01-27 18:41:42 -0500

    Initial Commit

---
+++ AndroidParseToDoList/app/src/main/java/com/joeymccoy/androidparsetodolist/App.java
@@ -0,0 +1,13 @@
+package com.joeymccoy.androidparsetodolist;
+
+import android.app.Application;
+import com.parse.Parse;
+
+public class App extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(this, "3fEGc93Zj6vaS8fIlv8v3InimTKmQf71zeBHbCSh", "2lw7gvS8Wj2UzpEaGzOp3I4zRMKrPZ25F3vK9Awr"); // Your Application ID and Client Key are defined elsewhere
+    }
+}
---
+++ AndroidParseToDoList/app/src/main/java/model/TasksAdapter.java
@@ -0,0 +1,32 @@
+package model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.joeymccoy.androidparsetodolist.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Dell on 11/6/2015.
+ */
+public class TasksAdapter extends ArrayAdapter<Task> {
+    public TasksAdapter(Context context, ArrayList<Task> tasks) {
+        super(context, 0, tasks);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Task task = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_task, parent, false);
+        }
+        TextView txtName = (TextView) convertView.findViewById(R.id.txtName);
+        txtName.setText(task.name);
+        return convertView;
+    }
+}
